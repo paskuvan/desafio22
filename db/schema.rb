2176b732cfea66825186cbd5be47ab7141c5a2a0
paskuvan <majopaskuvan@gmail.com>
2018-07-12 20:18:44 -0400

    update

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_07_131830) do
+ActiveRecord::Schema.define(version: 2018_07_12_235932) do
 
   create_table "students", force: :cascade do |t|
     t.string "name"
     t.integer "age"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "email"
   end
 
 end
